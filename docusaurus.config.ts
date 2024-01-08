96b2d90c473be57ed9306c7ddbc9a537d82c3302
0TickPulse <0tickpulse@gmail.com>
2024-01-08 21:48:31 +0800

    Update GitHub deployment config in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,9 @@ const config: Config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: "facebook", // Usually your GitHub org/user name.
-    projectName: "docusaurus", // Usually your repo name.
+    organizationName: "0tickpulse", // Usually your GitHub org/user name.
+    projectName: "megizen-docs", // Usually your repo name.
+    trailingSlash: false,
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
